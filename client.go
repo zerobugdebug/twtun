91afb97d1f2f66e1c4564d0ae81a0a8737e03a0c
zerobugdebug <github@zerobugdebug.ca>
2024-12-10 11:36:30 -0500

    Added debug and trace logging

--- client.go
+++ client.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 
 	"github.com/gorilla/websocket"
+
 )
 
 type Client struct {
@@ -20,20 +21,23 @@ func (c *Client) Run() error {
 	if err != nil {
 		return err
 	}
-	log.Printf("Listening for TCP connections on %s", c.tcpAddr)
+	log.Infof("TCP listener started on %s", c.tcpAddr)
+	log.Debugf("Client configuration: WebSocket address=%s, Proxy=%v", c.wsAddr, c.proxy != "")
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
+			log.Errorf("Failed to accept connection: %v", err)
 			continue
 		}
+		log.Debugf("New TCP connection accepted from %s", conn.RemoteAddr())
 		go c.handleConnection(conn)
 	}
 }
 
 func (c *Client) handleConnection(conn net.Conn) {
 	defer conn.Close()
+	log.Debugf("Handling new connection from %s", conn.RemoteAddr())
 
 	dialer := websocket.Dialer{
 		TLSClientConfig: &tls.Config{
@@ -44,54 +48,70 @@ func (c *Client) handleConnection(conn net.Conn) {
 	if c.proxy != "" {
 		proxyURL, err := url.Parse(c.proxy)
 		if err != nil {
-			log.Printf("Invalid proxy URL: %v", err)
+			log.Errorf("Invalid proxy URL: %v", err)
 			return
 		}
 		dialer.Proxy = http.ProxyURL(proxyURL)
+		log.Debugf("Using proxy: %s", c.proxy)
 	}
 
 	wsURL := "wss://" + c.wsAddr + "/proxy"
+	log.Debugf("Attempting WebSocket connection to %s", wsURL)
 	wsConn, _, err := dialer.Dial(wsURL, nil)
 	if err != nil {
-		log.Printf("WebSocket connection failed: %v", err)
+		log.Errorf("WebSocket connection failed: %v", err)
 		return
 	}
 	defer wsConn.Close()
+	log.Infof("WebSocket connection established with %s", c.wsAddr)
 
 	errChan := make(chan error, 2)
 
 	// TCP -> WebSocket
 	go func() {
+		log.Debug("Starting TCP -> WebSocket forwarding")
 		for {
 			buf := make([]byte, 32*1024)
 			n, err := conn.Read(buf)
 			if err != nil {
+				log.Debugf("TCP read error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Read %d bytes from TCP connection", n)
+
 			if err := wsConn.WriteMessage(websocket.BinaryMessage, buf[:n]); err != nil {
+				log.Debugf("WebSocket write error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Forwarded %d bytes to WebSocket", n)
 		}
 	}()
 
 	// WebSocket -> TCP
 	go func() {
+		log.Debug("Starting WebSocket -> TCP forwarding")
 		for {
 			_, message, err := wsConn.ReadMessage()
 			if err != nil {
+				log.Debugf("WebSocket read error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Read %d bytes from WebSocket", len(message))
+
 			_, err = conn.Write(message)
 			if err != nil {
+				log.Debugf("TCP write error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Forwarded %d bytes to TCP", len(message))
 		}
 	}()
 
 	// Wait for error
-	<-errChan
+	err = <-errChan
+	log.Infof("Connection closed: %v", err)
 }
--- server.go
+++ server.go
@@ -16,63 +16,83 @@ type Server struct {
 
 func (s *Server) Run() error {
 	http.HandleFunc("/proxy", s.handleWebSocket)
-	log.Printf("Starting WebSocket server on %s", s.wsAddr)
+	log.Infof("WebSocket server starting on %s", s.wsAddr)
+	log.Debugf("Server configuration: TCP address=%s, Cert=%s, Key=%s",
+		s.tcpAddr, s.certFile, s.keyFile)
 	return http.ListenAndServeTLS(s.wsAddr, s.certFile, s.keyFile, nil)
 }
 
 func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	log.Debugf("New WebSocket connection request from %s", r.RemoteAddr)
+
 	upgrader := websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
 	}
 
 	wsConn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Printf("WebSocket upgrade failed: %v", err)
+		log.Errorf("WebSocket upgrade failed: %v", err)
 		return
 	}
 	defer wsConn.Close()
+	log.Infof("WebSocket connection established with %s", r.RemoteAddr)
 
+	log.Debugf("Attempting TCP connection to %s", s.tcpAddr)
 	tcpConn, err := net.Dial("tcp", s.tcpAddr)
 	if err != nil {
-		log.Printf("TCP connection failed: %v", err)
+		log.Errorf("TCP connection failed: %v", err)
 		return
 	}
 	defer tcpConn.Close()
+	log.Infof("TCP connection established with %s", s.tcpAddr)
 
 	errChan := make(chan error, 2)
 
 	// TCP -> WebSocket
 	go func() {
+		log.Debug("Starting TCP -> WebSocket forwarding")
 		for {
 			buf := make([]byte, 32*1024)
 			n, err := tcpConn.Read(buf)
 			if err != nil {
+				log.Debugf("TCP read error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Read %d bytes from TCP connection", n)
+
 			if err := wsConn.WriteMessage(websocket.BinaryMessage, buf[:n]); err != nil {
+				log.Debugf("WebSocket write error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Forwarded %d bytes to WebSocket", n)
 		}
 	}()
 
 	// WebSocket -> TCP
 	go func() {
+		log.Debug("Starting WebSocket -> TCP forwarding")
 		for {
 			_, message, err := wsConn.ReadMessage()
 			if err != nil {
+				log.Debugf("WebSocket read error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Read %d bytes from WebSocket", len(message))
+
 			_, err = tcpConn.Write(message)
 			if err != nil {
+				log.Debugf("TCP write error: %v", err)
 				errChan <- err
 				return
 			}
+			log.Tracef("Forwarded %d bytes to TCP", len(message))
 		}
 	}()
 
 	// Wait for error
-	<-errChan
+	err = <-errChan
+	log.Infof("Connection closed: %v", err)
 }
